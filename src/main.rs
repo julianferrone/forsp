223030568574ee8cfd7007f1935d7d97aa211330
Julian Ferrone <hi@julianferrone.com>
2026-01-01 23:26:27 +1100

    line up parsed/evaluated debug messages

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ fn main() {
     loop {
         let user_input = get_user_input("forsp> ");
         let expr: interpreter::Object = parser::read(parser::scan(&user_input)).unwrap().into();
-        println!("  parsed line: {expr:?}");
+        println!("     | parsed line: {expr:?}");
         let env = state.env.clone();
         state = state.eval(expr, env);
-        println!("  evaluated: {state}");
+        println!("     | evaluated: {state}");
     }
 }
