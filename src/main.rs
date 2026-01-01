b13230c2a78c98369665f2424ae7b7c628fa4826
Julian Ferrone <hi@julianferrone.com>
2026-01-01 23:20:23 +1100

    Create REPL to interact with Forsp

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-mod parser;
+use std::io::prelude::*;
 mod interpreter;
+mod parser;
+
+fn get_user_input(prompt: &str) -> String {
+    let mut buf = String::new();
+    print!("{}", prompt);
+    std::io::stdout().flush().unwrap();
+    let stdin = std::io::stdin();
+    let _line = stdin.read_line(&mut buf).unwrap();
+    return buf;
+}
 
 fn main() {
-    let scanned = parser::scan("($n ($f $x ($u ^x) ($g ($h ^f g h)) n $k ($x ^x) k)) $pred");
-    println!("Scanned: {scanned:?}");
+    let mut state = interpreter::State::new();
+    loop {
+        let user_input = get_user_input("forsp> ");
+        let expr: interpreter::Object = parser::read(parser::scan(&user_input)).unwrap().into();
+        println!("  parsed line: {expr:?}");
+        let env = state.env.clone();
+        state = state.eval(expr, env);
+        println!("  evaluated: {state}");
+    }
 }
