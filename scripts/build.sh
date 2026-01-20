3c4e67b4f8bb5534179df5acb8f313e482bed4dc
Julian Ferrone <hi@julianferrone.com>
2026-01-20 15:56:54 +1100

    Add build and deployment scripts

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wasm-pack build --out-dir www/pkg --target no-modules
---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ -f .env ] then
+  . ./.env
+else
+  echo ".env file not found"
+  exit 1
+fi
+
+: "${DEPLOY_HOST:?DEPLOY_HOST not set}"
+: "${DEPLOY_PATH:?DEPLOY_PATH not set}"
+
+rsync --delete --archive www/ "${DEPLOY_HOST}:${DEPLOY_PATH}"
+
+echo "Uploaded files to VPS."
