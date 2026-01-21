b8f4fefadc9c5e6a16b471086d3755829973148c
Julian Ferrone <hi@julianferrone.com>
2026-01-21 18:41:08 +1100

    Implement Display for Primitive

--- src/primitive.rs
+++ src/primitive.rs
@@ -19,10 +19,34 @@ pub enum Primitive {
     Div,
 }
 
+
+
 pub trait ApplyPrimitive {
     fn apply_primitive(self, primitive: Primitive) -> Self;
 }
 
 pub trait ApplyPrimitiveMut {
-    fn apply_primitive(&mut self, primitive: Primitive);
+    fn apply_primitive(&mut self, primitive: Primitive) -> Result<(), String>;
+}
+
+impl std::fmt::Display for Primitive {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Primitive::Push => write!(f, "push"),
+            Primitive::Pop => write!(f, "pop"),
+            Primitive::Cons => write!(f, "cons"),
+            Primitive::Car => write!(f, "car"),
+            Primitive::Cdr => write!(f, "cdr"),
+            Primitive::Equals => write!(f, "eq"),
+            Primitive::Cswap => write!(f, "cswap"),
+            Primitive::Print => write!(f, "print"),
+            Primitive::Stack => write!(f, "stack"),
+            Primitive::Env => write!(f, "env"),
+            Primitive::Add => write!(f, "+"),
+            Primitive::Sub => write!(f, "-"),
+            Primitive::Mul => write!(f, "*"),
+            Primitive::Div => write!(f, "/"),
+            Primitive::Help => write!(f, "help"),
+        }
+    }
 }
